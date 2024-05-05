82175ee21a5acb6705f83706789adbc900a3198f
candySad <1121090320@qq.com>
2024-05-05 23:28:50 +0800

    merge

---
+++ 857.雇佣-k-名工人的最低成本.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=857 lang=python3
+#
+# [857] 雇佣 K 名工人的最低成本
+#
+
+# @lc code=start
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+# @lc code=end
+
